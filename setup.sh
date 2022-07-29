a33782815232fd40e5fdb7ac29960e8b0f360847
John Shimek <jshimek@backblaze.com>
2022-07-29 15:25:00 -0500

    Update setup

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Installing all the things"
-brew install vim git go gradle groovy httpie jq lolcat python3 tree watch
+brew install neovim git go gradle groovy httpie jq lolcat python3 tree watch
 
 echo "Setting up dotfiles"
 python dash.py
@@ -19,7 +19,6 @@ echo "Firefox: https://www.mozilla.org/en-US/firefox/download/thanks/"
 echo "Dropbox: https://www.dropbox.com/downloading"
 echo "1Password: https://1password.com/downloads/"
 echo "Alfred: https://www.alfredapp.com/"
-echo "Atom: https://atom.io/"
 echo "VS Code: https://code.visualstudio.com/"
 echo "Docker: https://www.docker.com/products/docker-desktop"
 echo "Chrome: https://www.google.com/chrome/"
@@ -27,5 +26,4 @@ echo "iTerm2: https://www.iterm2.com/downloads.html"
 echo "JetBrains Toolbox: https://www.jetbrains.com/toolbox/app/"
 echo "Postman: https://www.getpostman.com/"
 echo "Slack: https://slack.com/downloads/osx"
-echo "Spotify: https://www.spotify.com/us/download/mac/"
 
